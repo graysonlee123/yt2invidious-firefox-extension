13856efdfe00db523d711c8da27dbb03da67b944
Grayson Gantek <graysonleegantek@gmail.com>
2024-07-06 17:34:18 -0400

    Add and configures Rollup
    Adds Rollup, a bundler, in order to have more control over modules, plus additional features such as minification.

---
+++ rollup.config.mjs
@@ -0,0 +1,16 @@
+import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+  },
+  plugins: [typescript(), terser()],
+}
+
+export default config
