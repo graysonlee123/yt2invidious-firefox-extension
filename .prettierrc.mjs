96d5bc93f5963f01727ca7a24baabcf36a5545d2
Grayson Gantek <graysonleegantek@gmail.com>
2024-07-06 15:18:00 -0400

    Add configuration files for Prettier.
    Adds Prettier configuration and Prettier ignore file, which will ignore future TS config file.

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+export default {
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+}
